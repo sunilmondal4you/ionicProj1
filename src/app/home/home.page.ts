e442796dcc5b4cb204118d79b4f566b8bfff80ec
sunilmondal4you <sunilmondal4you@gmail.com>
2020-02-28 19:53:25 +0530

    change

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 // import { NavController } from 'ionic-angular';
+import { ToastController } from '@ionic/angular';
 
 import { FirebaseX } from "@ionic-native/firebase-x/ngx";
 
@@ -14,22 +15,40 @@ export class HomePage {
   myVariable: string = 'The force is with me!';
   myToken:any;
   
-  constructor(private firebase: FirebaseX) { }
+  constructor(private firebaseX: FirebaseX,public toastController: ToastController) { }
 
+  async presentToast(contData) {
+    const toast = await this.toastController.create({
+      message: contData,
+      duration: 2000
+    });
+    toast.present();
+  }
+    
   updateMyValue() {
+    this.presentToast("1")
+
     this.togglePoint = !this.togglePoint;
     if(this.togglePoint) this.myVariable = 'Now the force is even stronger!';
     else this.myVariable = 'The force is with me!';
 
-    this.firebaseX.getToken()
-      .then(token => console.log(`The token is ${token}`)) // save the token server-side and use it to push notifications to this device
-      .catch(error => console.error('Error getting token', error));
+    this.presentToast("2")
+
+    
+    // this.firebaseX.getToken()
+    //   .then(token => this.presentToast(`The token is ${token}`)) // save the token server-side and use it to push notifications to this device
+    //   .catch(error => this.presentToast('Error getting token', error));
 
     this.firebaseX.onMessageReceived()
-      .subscribe(data => console.log(`User opened a notification ${data}`));
+      .subscribe(
+        (data) => this.presentToast(`User opened a notification ${data}`),
+        (error) => { this.presentToast(error)});
 
     this.firebaseX.onTokenRefresh()
-      .subscribe((token: string) => console.log(`Got a new token ${token}`));
-  }
+      .subscribe(
+        (token: string) => this.presentToast(`Got a new token ${token}`),
+        (error) => this.presentToast(error));
 
+    
+  }
 }
